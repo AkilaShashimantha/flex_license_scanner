0bf820539f50d47a7de5f81fca7276357417478f
Akila Shashimantha <akilashashimantha84@gmail.com>
2026-01-09 00:00:30 +0530

    update

--- lib/flex_license_scanner.dart
+++ lib/flex_license_scanner.dart
@@ -3,7 +3,7 @@ import 'src/domain/entities/scanner_config.dart';
 import 'src/presentation/license_painter.dart';
 
 export 'src/domain/entities/scanner_config.dart';
-
+export 'src/data/datasources/license_ocr_service.dart';
 /// A professional overlay for Driving License OCR scanning.
 class LicenseScannerOverlay extends StatelessWidget {
   final ScannerConfig? config;
